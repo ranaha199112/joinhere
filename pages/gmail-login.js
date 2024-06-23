1a1d81ae7a237b052911aa42c6e85b27e5622fc6
Zayn Yusuf <yusue786@gmail.com>
2024-06-23 14:03:28 +0600

    adding router

--- pages/gmail-login.js
+++ pages/gmail-login.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import useMockLogin from "../hooks/useMockLogin";
 import { site } from "../config/index";
+import { useRouter } from "next/router";
 
 const UserIcon = () => {
   return (
@@ -48,7 +49,7 @@ export default function GmailLogin() {
   const [field, setField] = useState("email");
   const [showPassword, setShowPassword] = useState(false);
   const [nextPage, setNextPage] = useState(false);
-
+  const router = useRouter();
   const { login } = useMockLogin();
 
   const handleSubmit = (e) => {
